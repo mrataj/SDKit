4e4efd256be09ee164dceb977b743c01847d3e2c
Miha Rataj <rataj.miha@gmail.com>
2012-02-02 07:49:59 +0100

    bb code parsing support

---
+++ SDKit/SDKit/SDBulletinBoardParser.h
@@ -0,0 +1,13 @@
+//
+//  SDBulletinBoardParser.h
+//  SDKit
+//
+//  Created by Miha Rataj on 1.2.12.
+//  Copyright (c) 2012 Marg, d.o.o. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDBulletinBoardParser : NSObject
+
+@end
---
+++ SDKit/SDKit/SDBulletinBoardParser.m
@@ -0,0 +1,13 @@
+//
+//  SDBulletinBoardParser.m
+//  SDKit
+//
+//  Created by Miha Rataj on 1.2.12.
+//  Copyright (c) 2012 Marg, d.o.o. All rights reserved.
+//
+
+#import "SDBulletinBoardParser.h"
+
+@implementation SDBulletinBoardParser
+
+@end
