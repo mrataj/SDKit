04c9fb10fa7a489d140436c1d69bfd50800e1beb
Miha Rataj <rataj.miha@gmail.com>
2012-02-04 23:47:26 +0100

    default settings

--- SDKit/SDKit/SDButton.h
+++ SDKit/SDKit/SDButton.h
@@ -8,8 +8,20 @@
 
 #import "SDControl.h"
 
+@class SDEvent;
+
 @interface SDButton : SDControl {
-    
+    CGSize _size;
+    SDEvent *_event;
+    UIFont *_font;
+    UIColor *_textColor;
+    UIColor *_backgroundColor;
 }
+
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, retain) SDEvent *event;
+@property (nonatomic, retain) UIFont *font;
+@property (nonatomic, retain) UIColor *textColor;
+@property (nonatomic, retain) UIColor *backgroundColor;
 
 @end
--- SDKit/SDKit/SDButton.m
+++ SDKit/SDKit/SDButton.m
@@ -8,15 +8,22 @@
 
 #import "SDButton.h"
 #import "SDHelper.h"
+#import "SDEvent.h"
 
 @implementation SDButton
+
+@synthesize size=_size, event=_event, font=_font, textColor=_textColor, backgroundColor=_backgroundColor;
 
 - (id)init
 {
     self = [super init];
     if (self)
     {
-        
+        _size = CGSizeMake(50, 24);
+        _event = nil;
+        _font = [[UIFont boldSystemFontOfSize:14.0] retain];
+        _textColor = [[UIColor whiteColor] retain];
+        _backgroundColor = [[UIColor colorWithRed:59.0/255.0 green:89.0/255.0 blue:152.0/255.0 alpha:1.0] retain];
     }
     return self;
 }
@@ -38,8 +45,18 @@
     return CGSizeMakeFromPoint(CGSubstractTwoPoints(endpoint, point));
 }
 
+- (void)touchEndedAtLocation:(CGPoint)location
+{
+    [super touchEndedAtLocation:location];
+    [_event performEvent];
+}
+
 - (void)dealloc
 {
+    [_event release];
+    [_font release];
+    [_textColor release];
+    [_backgroundColor release];
     [super dealloc];
 }
 @end
