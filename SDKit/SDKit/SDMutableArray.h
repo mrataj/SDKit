b4786058ef0d0c86203e51c74dd43379f71cb192
Miha Rataj <rataj.miha@gmail.com>
2012-02-06 10:17:56 +0100

    added sdmutablearray

---
+++ SDKit/SDKit/SDMutableArray.h
@@ -0,0 +1,13 @@
+//
+//  SDMutableArray.h
+//  SDKit
+//
+//  Created by Miha Rataj on 6.2.12.
+//  Copyright (c) 2012 Marg, d.o.o. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDMutableArray : NSMutableArray
+
+@end
